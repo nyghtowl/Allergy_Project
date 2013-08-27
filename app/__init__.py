02f18254975646c1f4140f9fdee3bd2605065e33
nyghtowl <warrick.melanie@gmail.com>
2013-08-27 16:46:04 -0700

    Added OID

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+#implemented for oid
+from flask.ext.openid import OpenID 
+from config import basedir
 
 
 # Initialize Flask app
@@ -15,6 +18,7 @@ db = SQLAlchemy(app)
 # Login information
 login_manager = LoginManager()
 login_manager.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp')) #oid
 
 # Redirect non-loggedin users to login screen
 login_manager.login_view = "login" # result if user not logged in
