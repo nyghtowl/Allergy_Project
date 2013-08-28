579c365251d3da43ff0e0c7976d5c98bc0b1ca66
nyghtowl <warrick.melanie@gmail.com>
2013-08-27 22:14:01 -0700

    Added edit profile.

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
-from flask.ext.wtf import Form, TextField, BooleanField, TextAreaField
-from flask.ext.wtf import Required, Length
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import Required, Length
 
 class LoginForm(Form):
 	openid = TextField('openid', validators = [Required()])
 	remember_me = BooleanField('remember_me', default = False)
 
-class EditProfile(Form):
+class EditForm(Form):
     nickname = TextField('nickname', validators = [Required()])
     about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
 
